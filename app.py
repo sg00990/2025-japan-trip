97f5c191bbeec17cfb906f48e3436558237d3227
Sarah Graddy <92331459+sg00990@users.noreply.github.com>
2025-11-21 18:03:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 with st.sidebar:
-  col1, col2, col3 = st.columns(3)
+  col1, col2, col3 = st.columns([1, 3, 1])
   col2.header("My Japan Trip")
   col2.write("*November 1-12, 2025*")
