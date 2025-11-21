19030dbd01f29c60fe5dfebae5a03ecbedd927b3
Sarah Graddy <92331459+sg00990@users.noreply.github.com>
2025-11-21 18:05:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 
+
+st.set_page_config(
+    page_title="2025 Japan Trip Blog",
+    page_icon="🇯🇵",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
 with st.sidebar:
-  col1, col2, col3 = st.columns([1, 3, 1])
-  col2.header("My Japan Trip")
+  col1, col2, col3 = st.columns([.5, 3, .5])
+  col2.header("2025 Tokyo Blog")
   col2.write("*November 1-12, 2025*")
