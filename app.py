a7c6dc1b78f2baa9bb971f86cf5a294392778198
Sarah Graddy <92331459+sg00990@users.noreply.github.com>
2025-11-27 21:30:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ def open_image_correct_orientation(path):
 
 # main content
 st.title("**2025 Japan Trip Blog**")
-st.write("*November 1-12*")
+st.caption("*November 1-12*")
 
 tab1, tab2, tab3, tab4 = st.tabs(["Map", "Itinerary", "Highlights", "Gallery"])
 
