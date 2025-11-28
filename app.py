36400819a003693f4f5accec401ca056dc8e4230
Sarah Graddy <92331459+sg00990@users.noreply.github.com>
2025-11-27 20:00:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -190,5 +190,5 @@ with tab4:
     for i, p in enumerate(photos):
         img_path = os.path.join(folder, p)  # full path to the image
         img = Image.open(img_path)
-        im = ImageOps.exif_transpose(im)
+        img = ImageOps.exif_transpose(img)
         cols[i % 2].image(img, use_column_width=True)
